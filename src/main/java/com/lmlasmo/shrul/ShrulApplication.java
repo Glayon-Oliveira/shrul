c4377f61f1774cbf5f42e9f3e25c9c4efe4dc91e
Solansra <solansra@gmail.com>
2025-01-02 15:03:40 -0300

    Init

---
+++ src/main/java/com/lmlasmo/shrul/ShrulApplication.java
@@ -0,0 +1,13 @@
+package com.lmlasmo.shrul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShrulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShrulApplication.class, args);
+	}
+
+}
