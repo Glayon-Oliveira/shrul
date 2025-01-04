05ccd7cb2a16b4746660a9b23d4e212700523cac
Solansra <solansra@gmail.com>
2025-01-04 19:43:21 -0300

    Definir repositórios

---
+++ src/main/java/com/lmlasmo/shrul/repository/PrefixRepository.java
@@ -0,0 +1,13 @@
+package com.lmlasmo.shrul.repository;
+
+import java.math.BigInteger;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lmlasmo.shrul.model.Prefix;
+
+public interface PrefixRepository extends JpaRepository<Prefix, BigInteger>{
+
+	public boolean existsByPrefix(String prefix);
+	
+}
