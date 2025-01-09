47fa2686d170e3af5eddf7a7377177ee7d6426a5
Solansra <solansra@gmail.com>
2025-01-09 12:56:16 -0300

    Definir controles, serviços e DTOs referentes à Prefix

--- src/main/java/com/lmlasmo/shrul/repository/PrefixRepository.java
+++ src/main/java/com/lmlasmo/shrul/repository/PrefixRepository.java
@@ -2,6 +2,8 @@ package com.lmlasmo.shrul.repository;
 
 import java.math.BigInteger;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lmlasmo.shrul.model.Prefix;
@@ -9,5 +11,9 @@ import com.lmlasmo.shrul.model.Prefix;
 public interface PrefixRepository extends JpaRepository<Prefix, BigInteger>{
 
 	public boolean existsByPrefix(String prefix);
+	
+	public boolean existsByUserIdAndPrefixIsNull(BigInteger id);
+
+	public Page<Prefix> findByUserId(BigInteger id, Pageable pageable);
 	
 }
---
+++ src/main/java/com/lmlasmo/shrul/service/PrefixService.java
@@ -0,0 +1,78 @@
+package com.lmlasmo.shrul.service;
+
+import java.math.BigInteger;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lmlasmo.shrul.dto.PrefixDTO;
+import com.lmlasmo.shrul.dto.register.PrefixUpdateDTO;
+import com.lmlasmo.shrul.model.Prefix;
+import com.lmlasmo.shrul.repository.PrefixRepository;
+
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+@Transactional
+public class PrefixService {
+	
+	private PrefixRepository repository;
+	
+	public PrefixService(PrefixRepository repository) {
+		this.repository = repository;
+	}
+
+	public PrefixDTO save(PrefixDTO prefixDTO) {
+		
+		if(prefixDTO.getPrefix() == null && repository.existsByUserIdAndPrefixIsNull(prefixDTO.getUser())) {
+			throw new EntityExistsException("Null prefix field exists");
+		}
+		
+		Prefix prefix = new Prefix(prefixDTO);		
+		prefix = repository.save(prefix);
+		
+		return new PrefixDTO(prefix);
+	}
+
+	public PrefixDTO update(PrefixUpdateDTO update) {
+		
+		Optional<Prefix> prefixOp = repository.findById(update.getId());
+		
+		if(prefixOp.isPresent()) {
+			
+			Prefix prefix = prefixOp.get();			
+			
+			if(!repository.existsByPrefix(update.getPrefix())) {
+				
+				prefix.setPrefix(update.getPrefix());
+				
+				prefix = repository.save(prefix);
+				
+				return new PrefixDTO(prefix);
+			}
+			
+			throw new EntityExistsException("Prefix exists");			
+		}
+		
+		throw new EntityNotFoundException("Prefix not found");		
+	}
+
+	public boolean delete(BigInteger id) {
+		
+		if(!repository.existsById(id)) return false;
+		
+		repository.deleteById(id);
+		
+		return !repository.existsById(id);
+	}
+
+	public Page<PrefixDTO> findById(BigInteger id, Pageable pageable) {	
+		
+		return repository.findByUserId(id, pageable).map(p -> new PrefixDTO(p));
+	}
+
+}
