d99fc57806e8b92d538f46bcea916f05be1eaec9
Solansra <solansra@gmail.com>
2025-01-13 13:11:28 -0300

    Definir controller para redirecionamento de url

---
+++ src/main/java/com/lmlasmo/shrul/controller/RedirectController.java
@@ -0,0 +1,36 @@
+package com.lmlasmo.shrul.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.lmlasmo.shrul.service.LinkService;
+
+@RestController
+public class RedirectController {
+
+	private LinkService service;
+	
+	public RedirectController(LinkService service) {
+		this.service = service;
+	}
+	
+	@GetMapping("/{id}")
+	public Object redirect(@PathVariable String id){
+		
+		String destine = service.getDestination(id);		
+	
+		return (destine != null) ? new RedirectView(destine) : ResponseEntity.notFound().build();
+	}
+	
+	@GetMapping("/{prefix}/{id}")
+	public Object redirect(@PathVariable String id, @PathVariable String prefix){
+		
+		String destine = service.getDestination(id, prefix);
+		
+		return (destine != null) ? new RedirectView(destine) : ResponseEntity.notFound().build();
+	}
+	
+}
