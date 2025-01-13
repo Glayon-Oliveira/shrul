bb62ce8fee8287213410f97db2a382aeb7c291ad
Solansra <solansra@gmail.com>
2025-01-13 18:28:07 -0300

    Definir lógica de login

--- src/main/java/com/lmlasmo/shrul/controller/UserController.java
+++ src/main/java/com/lmlasmo/shrul/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.lmlasmo.shrul.controller;
 
 import java.math.BigInteger;
+import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,9 +17,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lmlasmo.shrul.dto.JwtTokenDTO;
 import com.lmlasmo.shrul.dto.UserDTO;
+import com.lmlasmo.shrul.dto.register.LoginDTO;
 import com.lmlasmo.shrul.dto.register.SignupDTO;
 import com.lmlasmo.shrul.dto.register.UserUpdateDTO;
+import com.lmlasmo.shrul.service.JwtService;
 import com.lmlasmo.shrul.service.UserService;
 
 import jakarta.validation.Valid;
@@ -24,16 +31,34 @@ import jakarta.validation.Valid;
 @RequestMapping("/api/user")
 public class UserController{
 
-	private UserService service;
+	private UserService userService;
+	private JwtService jwtService;
+	private AuthenticationManager manager;	
 	
-	public UserController(UserService service) {
-		this.service = service;
+	public UserController(UserService userService, JwtService jwtService, AuthenticationManager manager) {
+		this.userService = userService;		
+		this.jwtService = jwtService;
+		this.manager = manager;
+	}
+	
+	@PostMapping("/login")
+	public ResponseEntity<JwtTokenDTO> login(@RequestBody @Valid LoginDTO login){
+		
+		UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(login.getEmail(), login.getPassword());
+		
+		Authentication auth = manager.authenticate(authToken);		
+		
+		List<String> authotities = auth.getAuthorities().stream().map(a -> a.getAuthority()).toList(); 
+		
+		String jwtToken = jwtService.gerateToken(auth.getName(), authotities);
+		
+		return ResponseEntity.ok(new JwtTokenDTO(jwtToken));
 	}
 		
 	@PostMapping("/signup")	
 	public ResponseEntity<UserDTO> signup(@RequestBody @Valid SignupDTO signup){
 		
-		UserDTO user = service.save(signup);
+		UserDTO user = userService.save(signup);
 		
 		if(user != null) {
 			return ResponseEntity.ok(user);
@@ -45,7 +70,7 @@ public class UserController{
 	@PutMapping
 	public ResponseEntity<UserDTO> update(@RequestBody @Valid UserUpdateDTO update){
 		
-		UserDTO user = service.update(update);
+		UserDTO user = userService.update(update);
 		
 		if(user != null) {
 			return ResponseEntity.ok(user);
@@ -57,14 +82,14 @@ public class UserController{
 	@PutMapping("/lock")
 	public ResponseEntity<Object> setLocked(@RequestParam BigInteger id, @RequestParam boolean locked){
 		
-		service.setLocked(id, locked);
+		userService.setLocked(id, locked);
 		
 		return ResponseEntity.ok().build();
 	}
 	
 	@GetMapping
 	public Page<UserDTO> findAll(Pageable pageable){
-		return service.getRepository().findAll(pageable)
+		return userService.getRepository().findAll(pageable)
 				.map(u -> new UserDTO(u));
 	}
 	
