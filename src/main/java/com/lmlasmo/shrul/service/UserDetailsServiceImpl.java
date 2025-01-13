26920dcd3da6861c8b495f671abdb1b0df227065
Solansra <solansra@gmail.com>
2025-01-13 18:02:29 -0300

    Definir implementação do UserDetailsService, usando do email como username

---
+++ src/main/java/com/lmlasmo/shrul/service/UserDetailsServiceImpl.java
@@ -0,0 +1,40 @@
+package com.lmlasmo.shrul.service;
+
+import java.util.Optional;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lmlasmo.shrul.model.User;
+import com.lmlasmo.shrul.repository.UserRepository;
+
+@Service
+@Transactional
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	private UserRepository repository;
+	
+	public UserDetailsServiceImpl(UserRepository repository) {
+		this.repository = repository;
+	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return loadUserByEmail(username);		
+	}
+	
+	public UserDetails loadUserByEmail(String email) throws UsernameNotFoundException{
+	
+		Optional<User> user = repository.findByEmail(email);
+		
+		if(user.isPresent()) {
+			return user.get();
+		}
+		
+		throw new UsernameNotFoundException("User not found");
+	}	
+
+}
