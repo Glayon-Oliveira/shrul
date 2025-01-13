f2a0e8403b9c28f28c974e0f60a2ac946e331e0f
Solansra <solansra@gmail.com>
2025-01-13 19:58:06 -0300

    Definir lógica de login por JWT

---
+++ src/main/java/com/lmlasmo/shrul/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,86 @@
+package com.lmlasmo.shrul.filter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.lmlasmo.shrul.model.User;
+import com.lmlasmo.shrul.repository.UserRepository;
+import com.lmlasmo.shrul.service.JwtService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+	
+	private UserRepository repository;
+	private JwtService jwtService;
+	
+	@Autowired
+	public JwtAuthenticationFilter(UserRepository repository, JwtService jwtService) {
+		this.repository = repository;
+		this.jwtService = jwtService;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		String token = findToken(request);
+		
+		if(token != null) {		
+			authenticate(token);		
+		}
+				
+		filterChain.doFilter(request, response);
+	}
+	
+	private void authenticate(String token) {
+	
+		Optional<User> userOp = findUser(token);
+		
+		if(userOp.isPresent()) {
+			
+			User user = userOp.get();
+			
+			if(isAuthenticationPermited(user)) {
+				
+				UsernamePasswordAuthenticationToken authToken = UsernamePasswordAuthenticationToken.authenticated(user, null, user.getAuthorities());					
+				SecurityContextHolder.getContext().setAuthentication(authToken);
+			}
+			
+		}
+		
+	}
+	
+	private String findToken(HttpServletRequest request) {
+		
+		String auth = request.getHeader("Authorization");
+		
+		if(auth != null) {			
+			auth = auth.replace("Bearer ", "");						
+		}
+		
+		return auth;		
+	}
+	
+	private Optional<User> findUser(String token) {
+		
+		String email = jwtService.getEmail(token);
+				
+		return (email != null) ? repository.findByEmail(email) : Optional.ofNullable(null); 		 
+	}
+	
+	private boolean isAuthenticationPermited(User user) {		
+		return !user.isLocked() && user.isEnabled();		
+	}
+
+}
--- src/main/java/com/lmlasmo/shrul/infra/security/SecurityConfig.java
+++ src/main/java/com/lmlasmo/shrul/infra/security/SecurityConfig.java
@@ -8,9 +8,13 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import com.lmlasmo.shrul.filter.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -18,14 +22,16 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtFilter) throws Exception {
 		
 		return http.csrf(c -> c.disable())
+				   .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				   .formLogin(f -> f.disable())
 				   .httpBasic(Customizer.withDefaults())				   
 				   .authorizeHttpRequests(a -> a.requestMatchers("/user/login", "/user/signup").permitAll()
 						   .requestMatchers("/user/**", "/prefix/**", "/link/**").authenticated()
 						   .anyRequest().permitAll())
+				   .addFilterBefore(jwtFilter, BasicAuthenticationFilter.class)
 				   .build();		
 	}
 	
