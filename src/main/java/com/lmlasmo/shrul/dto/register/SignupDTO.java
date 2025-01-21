ce7ff105d29679dacf12d6e5c08d6de80ed1447b
Solansra <solansra@gmail.com>
2025-01-20 23:11:14 -0300

    Relacionar classes por meio do 'extends'

--- src/main/java/com/lmlasmo/shrul/dto/register/SignupDTO.java
+++ src/main/java/com/lmlasmo/shrul/dto/register/SignupDTO.java
@@ -2,22 +2,10 @@ package com.lmlasmo.shrul.dto.register;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 
-public class SignupDTO {
-
-	@JsonProperty
-	@Email
-	@NotBlank
-	private String email;	
-
-	@JsonProperty
-	@NotBlank
-	@Size(min = 8, max = 255)
-	private String password;
+public class SignupDTO extends LoginDTO{
 	
 	@JsonProperty("first_name")	
 	@NotBlank
@@ -29,23 +17,7 @@ public class SignupDTO {
 	@Pattern(regexp = "^(?!\s).*[^\\s]$")
 	public String lastName;	
 	
-	public SignupDTO() {}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email.toUpperCase();
-	}	
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
+	public SignupDTO() {}	
 
 	public String getFirstName() {
 		return firstName;
