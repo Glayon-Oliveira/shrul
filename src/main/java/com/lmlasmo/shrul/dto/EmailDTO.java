e6f0302b3f3a4a219d471657f62b8460b52e57fa
Solansra <solansra@gmail.com>
2025-01-20 22:58:14 -0300

    Definir lógica de cadastro por meio de confirmação de email

---
+++ src/main/java/com/lmlasmo/shrul/dto/EmailDTO.java
@@ -0,0 +1,23 @@
+package com.lmlasmo.shrul.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class EmailDTO {
+
+	@JsonProperty
+	@NotBlank
+	private String email;
+	
+	public EmailDTO() {}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email.toUpperCase();
+	}
+		
+}
---
+++ src/main/java/com/lmlasmo/shrul/dto/register/SignupWithCodeHashDTO.java
@@ -0,0 +1,35 @@
+package com.lmlasmo.shrul.dto.register;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class SignupWithCodeHashDTO extends SignupDTO {
+
+	@JsonProperty
+	@NotBlank	
+	private String code;
+	
+	@JsonProperty
+	@NotBlank
+	private String hash;	
+	
+	public SignupWithCodeHashDTO() {}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getHash() {
+		return hash;
+	}
+
+	public void setHash(String hash) {
+		this.hash = hash;
+	}
+	
+}
