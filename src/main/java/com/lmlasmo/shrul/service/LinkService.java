9c47b498ca9967f9684d79460bdd8ed141b2b039
Solansra <solansra@gmail.com>
2025-01-26 17:23:36 -0300

    Tratar exceções e corrigir erros

--- src/main/java/com/lmlasmo/shrul/service/LinkService.java
+++ src/main/java/com/lmlasmo/shrul/service/LinkService.java
@@ -11,10 +11,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.lmlasmo.shrul.dto.model.LinkDTO;
+import com.lmlasmo.shrul.infra.erro.DestinationNotFoundException;
+import com.lmlasmo.shrul.infra.erro.GenericException;
 import com.lmlasmo.shrul.model.Link;
 import com.lmlasmo.shrul.repository.LinkRepository;
 import com.lmlasmo.shrul.util.LinkCodeCreator;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 
 @Service
@@ -30,7 +33,7 @@ public class LinkService {
 
 	public LinkDTO save(@Valid LinkDTO linkDTO) {
 
-		Link link = new Link(linkDTO);
+		Link link = new Link(linkDTO);		
 
 		String id = "0000000000";
 
@@ -51,50 +54,58 @@ public class LinkService {
 		return new LinkDTO(link);
 	}
 
-	public boolean delete(String id) {
+	public void delete(String id) {
 
 		if(!repository.existsById(id)) {
-			return false;
+			throw new EntityNotFoundException("Link not found");
 		}
 
 		repository.deleteById(id);
 
-		return !repository.existsById(id);
+		if(repository.existsById(id)) {
+			throw new GenericException("Link not deleted");
+		}
 	}
 
 	public LinkDTO findById(String id) {
 
 		Optional<LinkDTO> link = repository.findById(id).map(l -> new LinkDTO(l));
 
-		if(link.isPresent()) {
-			return link.get();
+		if(link.isEmpty()) {
+			throw new DestinationNotFoundException("Link not found");
 		}
 
-		return null;
+		return link.get();
 	}
 	
 	public Page<LinkDTO> findByUser(BigInteger userId, Pageable pageable) {
-
 		return repository.findByPrefixUserId(userId, pageable).map(l -> new LinkDTO(l));
 	}
 
 	public Page<LinkDTO> findByPrefix(BigInteger id, Pageable pageable) {
-
 		return repository.findByPrefixId(id, pageable).map(l -> new LinkDTO(l));
 	}
 
 	public String getDestination(String id) {
+
+		Optional<Link> link = repository.findById(id);
 		
-		Optional<Link> link = repository.findById(id);			
-		
-		return (link.isPresent()) ? link.get().getDestination() : null;
+		if((link.isEmpty())) {
+			throw new DestinationNotFoundException("Link not found");
+		}
+
+		return link.get().getDestination();
 	}
 
 	public String getDestination(String id, String prefix) {
 		
 		Link link = repository.findByIdAndPrefixPrefix(id, prefix);
 		
-		return (link != null) ? link.getDestination() : null;
+		if(link == null) {
+			throw new DestinationNotFoundException("Link not found");
+		}
+
+		return link.getDestination();
 	}
 
 }
