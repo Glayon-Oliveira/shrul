03874f5cd58f7c5e76c5f2552a59725723ba5ce0
Solansra <solansra@gmail.com>
2025-01-27 02:14:19 -0300

    Redefinir lógica da confirmação de email

--- src/main/java/com/lmlasmo/shrul/dto/CodeHashDTO.java
+++ src/main/java/com/lmlasmo/shrul/dto/CodeHashDTO.java
@@ -1,14 +1,45 @@
 package com.lmlasmo.shrul.dto;
 
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@JsonInclude(value = Include.NON_NULL)
 public class CodeHashDTO {
+	
+	@JsonProperty
+	@NotNull
+	private LocalDateTime timestamp;
 
 	@JsonProperty
+	@NotBlank
 	private String hash;
+	
+	@JsonProperty	
+	@NotBlank
+	@Size(min = 6, max = 6)
+	private String code;
+	
+	public CodeHashDTO() {}
 
-	public CodeHashDTO(String hash) {
+	public CodeHashDTO(LocalDateTime timestamp, String hash, String code) {
+		this.timestamp = timestamp;
 		this.hash = hash;
+		this.code = code;
+	}
+
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
 	}
 
 	public String getHash() {
@@ -18,5 +49,21 @@ public class CodeHashDTO {
 	public void setHash(String hash) {
 		this.hash = hash;
 	}
+
+	public String getCode() {
+		return code;
+	}
+	
+	public String getCodeAndSetNull() {
+		
+		String code = this.code;
+		this.code = null;
+		
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}	
 
 }
