ffc6bd91a364be0f437bd8a05e3afd740db7bf16
Solansra <solansra@gmail.com>
2025-01-27 02:14:19 -0300

    Redefinir lógica da confirmação de email

--- src/main/java/com/lmlasmo/shrul/dto/register/PasswordUpdateDTO.java
+++ src/main/java/com/lmlasmo/shrul/dto/register/PasswordUpdateDTO.java
@@ -1,27 +1,25 @@
 package com.lmlasmo.shrul.dto.register;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.lmlasmo.shrul.dto.CodeHashDTO;
 import com.lmlasmo.shrul.dto.EmailDTO;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public class PasswordUpdateWithCodeHashDTO extends EmailDTO{
+public class PasswordUpdateDTO extends EmailDTO{
 
 	@JsonProperty
 	@NotBlank
 	@Size(min = 8, max = 255)
 	private String password;
 	
-	@JsonProperty
-	@NotBlank
-	private String hash;
+	@JsonProperty("code_hash")
+	@NotNull
+	private CodeHashDTO codeHash;	
 	
-	@JsonProperty
-	@NotBlank
-	private String code;
-	
-	public PasswordUpdateWithCodeHashDTO() {}
+	public PasswordUpdateDTO() {}
 
 	public String getPassword() {
 		return password;
@@ -31,20 +29,12 @@ public class PasswordUpdateWithCodeHashDTO extends EmailDTO{
 		this.password = password;
 	}
 
-	public String getHash() {
-		return hash;
+	public CodeHashDTO getCodeHash() {
+		return codeHash;
 	}
 
-	public void setHash(String hash) {
-		this.hash = hash;
-	}
-
-	public String getCode() {
-		return code;
-	}
-
-	public void setCode(String code) {
-		this.code = code;
+	public void setCodeHash(CodeHashDTO codeHash) {
+		this.codeHash = codeHash;
 	}	
 	
 }
--- src/main/java/com/lmlasmo/shrul/util/EmailCodeTool.java
+++ src/main/java/com/lmlasmo/shrul/util/EmailCodeTool.java
@@ -1,48 +1,92 @@
 package com.lmlasmo.shrul.util;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
 
-public interface EmailCodeTool {
+import com.lmlasmo.shrul.dto.CodeHashDTO;
+import com.lmlasmo.shrul.infra.erro.EmailConfirmationException;
 
-	public static Map<String, String> create(String email) {
-
-		String key = System.getenv("EMAIL_CONFIRMATION_KEY");
-
-		if(key == null) {
-			throw new IllegalStateException("Environment variable 'MY_ENV_VAR' is not set but is required.");
-		}
-
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
-		String code = Integer.toString(new Random().nextInt(10000000, 99999999));
-
-		String hash = encoder.encode(email+code+key);
-
-		Map<String, String> codeHash = new HashMap<>();
-
-		codeHash.put("code", code);
-		codeHash.put("hash", hash);
-
-		return Map.copyOf(codeHash);
+@Component
+public class EmailCodeTool {
+	
+	@Value("${EMAIL_CONFIRMATION_KEY}")
+	private String key;
+	
+	public static final int EXPIRES_MINUTES = 5;
+	private AtomicLong logicKey = new AtomicLong(0);
+	
+	public EmailCodeTool() {};
+	
+	private byte somaByte(long value) {
+		
+		byte[] bytes = Long.toString(value).getBytes();				
+		value = 0;		
+		
+		for(byte b: bytes) {
+			value += b;
+		}		
+		
+		return (byte) value;
+	}
+	
+	private byte setLogicKeyAndReturnLogicProduct(int code, LocalDateTime time){		
+				
+		long logicKey = this.logicKey.get();				
+		long miles = time.toInstant(ZoneOffset.UTC).toEpochMilli();		
+		int random = new Random().nextInt(1, 1000);				
+		
+		long toKey = miles/random;
+		logicKey += (Long.MAX_VALUE-random > toKey) ? -toKey : toKey;
+		logicKey = (Long.MAX_VALUE/code > logicKey) ? logicKey/code : logicKey*code;		
+		logicKey = Math.abs(logicKey);
+		
+		this.logicKey.set(logicKey);
+		
+		return somaByte(logicKey);		
+	}
+	
+	private String gerateCode(LocalDateTime timestamp) {
+		
+		int random = new Random().nextInt(1, 1000000);
+		int product = setLogicKeyAndReturnLogicProduct(random, timestamp);
+		
+		return String.format("%06d", random+product);
 	}
 
-	public static boolean confirm(String email, String code, String hash) {
+	public CodeHashDTO create(String email) {			
 
-		String key = System.getenv("EMAIL_CONFIRMATION_KEY");
-
-		if(key == null) {
-			throw new IllegalStateException("Environment variable 'MY_ENV_VAR' is not set but is required.");
-		}
-
-		String compiled = email+code+key;
-
+		LocalDateTime timestamp = LocalDateTime.now().withNano(0);
+		
+		String code = gerateCode(timestamp);
+		
+		String toHash = email+code+key+timestamp;		
+		
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
-		return encoder.matches(compiled, hash);
+		String hash = encoder.encode(toHash);		
+		
+		return new CodeHashDTO(timestamp, hash, code);
 	}
+
+	public void confirm(String email, CodeHashDTO codeHash) {				
+		
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		LocalDateTime timestamp = LocalDateTime.now().withNano(0);		
+		String compiled = email+codeHash.getCode()+key+codeHash.getTimestamp();		
+		
+		if(!encoder.matches(compiled, codeHash.getHash())) {
+			throw new EmailConfirmationException("Invalid code");
+		}
+				
+		if(timestamp.isAfter(codeHash.getTimestamp().plusMinutes(EXPIRES_MINUTES))) {
+			throw new EmailConfirmationException("Expired code");
+		}						
+		
+	}	
 
 }
